310ce9f7690f629bb8d295d136da424ab61de66e
Harrison <hmcguire13@gmail.com>
2016-04-28 17:55:14 -0700

    add server console log

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ var express = require('express'),
 	io = require('socket.io').listen(server),
 	nicknames = [];
 	
-server.listen(3000);
+server.listen(3000, function(){
+  console.log('listening on port:3000');
+});
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
